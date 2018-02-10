61be858dbc8893f3a3a374fedd92c64e9c7b1159
pwangdu <35709354+pwangdu@users.noreply.github.com>
2018-02-11 02:34:21 +0530

    Add files via upload

--- src/main/java/org/steemdata/graph/configuration/SteemConfig.java
+++ src/main/java/org/steemdata/graph/configuration/SteemConfig.java
@@ -1,19 +1,13 @@
 package org.steemdata.graph.configuration;
 
 import eu.bittrade.libs.steemj.SteemJ;
-import eu.bittrade.libs.steemj.base.models.AccountName;
-import eu.bittrade.libs.steemj.configuration.SteemJConfig;
-import eu.bittrade.libs.steemj.enums.PrivateKeyType;
 import eu.bittrade.libs.steemj.exceptions.SteemCommunicationException;
 import eu.bittrade.libs.steemj.exceptions.SteemResponseException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.steemdata.graph.Utils;
 
 /**
  * Created by Pankaj Wahane on 11-02-2018.
@@ -34,18 +28,6 @@ public class SteemConfig {
     @Bean
     SteemJ steemj() throws SteemCommunicationException, SteemResponseException {
         log.info(String.format("account(%s), postKey(%s), activeKey(%s)", accountName, postKey, activeKey));
-
-        SteemJConfig steemJConfig = SteemJConfig.getInstance();
-        steemJConfig.setResponseTimeout(100000);
-        steemJConfig.setDefaultAccount(new AccountName(accountName));
-
-        List<ImmutablePair<PrivateKeyType, String>> privateKeys = new ArrayList<>();
-        privateKeys.add(new ImmutablePair<>(PrivateKeyType.POSTING, postKey));
-        privateKeys.add(new ImmutablePair<>(PrivateKeyType.ACTIVE, activeKey));
-
-        steemJConfig.getPrivateKeyStorage().addAccount(steemJConfig.getDefaultAccount(), privateKeys);
-
-        SteemJ steemJ = new SteemJ();
-        return steemJ;
+        return Utils.getSteemJInstance(accountName, postKey, activeKey);
     }
 }
--- src/test/java/org/steemdata/graph/SteemDataGraphApplicationTests.java
+++ src/test/java/org/steemdata/graph/SteemDataGraphApplicationTests.java
@@ -1,16 +1,56 @@
 package org.steemdata.graph;
 
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import eu.bittrade.libs.steemj.communication.HttpClientRequestInitializer;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SteemDataGraphApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException, GeneralSecurityException {
+		createTestNetAccount("my-sd-tests","P8N6sHEJu438dj4dwY9jx9c8deeKpPA6XWCr9aTC5SQ7MiCjMUm");
 	}
-
+	/**
+	 * Create a new TestNet account as described in the TestNet main page
+	 * (https://testnet.steem.vc).
+	 *
+	 * @param username
+	 *            The account to create.
+	 * @param password
+	 *            The password to set for the <code>username</code>.
+	 * @throws IOException
+	 *             In case something went wrong.
+	 * @throws GeneralSecurityException
+	 *             In case something went wrong.
+	 */
+	private static void createTestNetAccount(String username, String password)
+			throws IOException, GeneralSecurityException {
+		NetHttpTransport.Builder builder = new NetHttpTransport.Builder();
+		// Disable SSL verification:
+		builder.doNotValidateCertificate();
+		HttpRequest httpRequest = builder.build().createRequestFactory(new HttpClientRequestInitializer())
+				.buildPostRequest(new GenericUrl("https://testnet.steem.vc/create"), ByteArrayContent.fromString(
+						"application/x-www-form-urlencoded", "username=" + username + "&password=" + password));
+		try {
+			httpRequest.execute();
+		} catch (HttpResponseException e) {
+			if (e.getStatusCode() != 409) {
+				log.info("Account already existed.");
+			}
+		}
+	}
 }
