3c15709395db659717031df49153d2c7f9c2cac1
pwangdu <35709354+pwangdu@users.noreply.github.com>
2018-02-11 00:41:48 +0530

    Spring Boot Project Structure

---
+++ src/main/java/org/steemdata/graph/SteemDataGraphApplication.java
@@ -0,0 +1,12 @@
+package org.steemdata.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteemDataGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteemDataGraphApplication.class, args);
+	}
+}
